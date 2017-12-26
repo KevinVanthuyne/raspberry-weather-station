31154d860dc8b561e2891012e3a34872a733d86f
KevinVanthuyne <kevin.vanthuyne2@gmail.com>
2017-12-26 22:18:01 +0100

    Added lightsensor
    Added a LM393 lightsensor to disable the screen when the light is off

--- weather.py
+++ weather.py
@@ -17,12 +17,15 @@ import Adafruit_DHT as dht
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Font, Alignment
 
+import RPi.GPIO as GPIO
+
 import time
 import os.path
 import datetime
 
-__excel_file_name = 'temperatures.xlsx'
-__DHT22_pin = 17  # GPIO pin on Raspberry Pi for DHT22 sensor (BCM number)
+__excel_file_name = '/home/pi/Programs/Weerstation/temperatures.xlsx'
+__DHT22_pin = 15  # GPIO pin on Raspberry Pi for DHT22 sensor (BCM number)
+__lightsensor_pin = 27  #GPIO pin connected to digital out of LM393 light sensor
 
 def run():
     # Matrix setup
@@ -37,58 +40,68 @@ def run():
     # Excel setup
     workbook = load_workbook(__excel_file_name)
 
+    # Light sensor setup
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(__lightsensor_pin, GPIO.IN)
+
+    # Show X on screen to inform Pi is booting
+    with canvas(device) as draw:
+        text(draw, (0, 0), "X", fill="white", font=proportional(TINY_FONT))
+
     # Main loop
     while True:
-        # Sleep between 24:00 and 6:00
         now = datetime.datetime.now()
-        # If it's later than 6:00
-        if now.hour >= 6 and now.minute >= 0:
-            print("It's now: {}".format(datetime.datetime.now().time()))
-            # Get current weather info for Leuven
-            # obs = owm.weather_at_coords(50.875494, 4.711183) # Observation
-            # Peizegem city:
-            obs = owm.weather_at_coords(50.978978, 4.211429)
+        print("It's now: {}".format(datetime.datetime.now().time()))
+        # Get current weather info for Leuven
+        # obs = owm.weather_at_coords(50.875494, 4.711183) # Observation
+        # Peizegem city:
+        obs = owm.weather_at_coords(50.978978, 4.211429)
 
-            # get outside weather
-            weather = obs.get_weather()
-            timestamp = weather.get_reference_time()
-            day = datetime.datetime.fromtimestamp(timestamp).strftime('%d/%m/%Y')
-            hour = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
-            outside_temp = weather.get_temperature(unit = 'celsius')['temp']
+        # get outside weather
+        weather = obs.get_weather()
+        timestamp = weather.get_reference_time()
+        day = datetime.datetime.fromtimestamp(timestamp).strftime('%d/%m/%Y')
+        hour = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
+        outside_temp = weather.get_temperature(unit = 'celsius')['temp']
 
-            # get inside sensor readings
-            inside_humid, inside_temp = dht.read_retry(dht.DHT22, __DHT22_pin, retries = 15, delay_seconds = 1)
-            # if a reading could be gotten
-            if inside_humid is not None and inside_temp is not None:
-                inside_humid = round(inside_humid, 2)
-                inside_temp = round(inside_temp, 2)
-            else:
-                inside_temp = ""
-                inside_humid = ""
+        # get inside sensor readings
+        inside_humid, inside_temp = dht.read_retry(dht.DHT22, __DHT22_pin, retries = 5, delay_seconds = 1)
+        # if a reading could be gotten
+        if inside_humid is not None and inside_temp is not None:
+            inside_humid = round(inside_humid, 2)
+            inside_temp = round(inside_temp, 2)
+        else:
+            inside_temp = ""
+            inside_humid = ""
 
-            # print some data
-            print("Fetched data is from: {} {}".format(day, hour))
-            print("it's now {}°C".format(outside_temp))
-            print("Short weather status: {}".format(weather.get_status()))
-            print("Detailed weather status: {}".format(weather.get_detailed_status()))
-            print("Sunrise: {}".format(weather.get_sunrise_time('iso')))
-            print("Sunset: {}".format(weather.get_sunset_time('iso')))
-            print("")
-            print("Inside temp: {}°C".format(inside_temp))
-            print("Inside humidity: {}%".format(inside_humid))
-            print("")
+        # print some data
+        print("Fetched data is from: {} {}".format(day, hour))
+        print("it's now {}°C".format(outside_temp))
+        print("Short weather status: {}".format(weather.get_status()))
+        print("Detailed weather status: {}".format(weather.get_detailed_status()))
+        print("Sunrise: {}".format(weather.get_sunrise_time('iso')))
+        print("Sunset: {}".format(weather.get_sunset_time('iso')))
+        print("")
+        print("Inside temp: {}°C".format(inside_temp))
+        print("Inside humidity: {}%".format(inside_humid))
+        print("")
 
-            # log data to Excel
-            write_to_excel(workbook, day, hour, outside_temp, inside_temp, inside_humid)
+        # log data to Excel
+        write_to_excel(workbook, day, hour, outside_temp, inside_temp, inside_humid)
 
+        # If it's not dark in the room
+        # (Sensor output = 1 if dark, 0 if light)
+        if not GPIO.input(__lightsensor_pin):
             # Display current temperature
             with canvas(device) as draw:
                 text(draw, (0, 0), str(outside_temp), fill="white", font=proportional(TINY_FONT))
-            # Sleep 10 minutes
-            time.sleep(10 * 60)
         else:
-            # sleep 30 minutes
-            time.sleep(30 * 60)
+            print("Device cleared")
+            device.clear()  # TODO device.show() and hide()
+
+        # Sleep 5 minutes
+        time.sleep(5 * 60)
+
 
 def create_excel():
     wb = Workbook()  # create workbook
