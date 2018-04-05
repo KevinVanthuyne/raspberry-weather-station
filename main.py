f44d5530b46d0891d1e214f546cbde482083edfc
KevinVanthuyne <kevin.vanthuyne2@gmail.com>
2018-04-05 18:06:56 +0200

    Changed screen wake up and sleep to an interrupt
    Screen now immediately goes to sleep or wakes up when a light change is detected

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import time
 import datetime
 from PIL import Image
+import RPi.GPIO as GPIO
 
 from weatherstation import WeatherStation
 from database import Database
@@ -19,6 +20,24 @@ db_file = 'weatherdata.db' # name of the database file
 # excel_file_name = '/home/pi/Programs/Weerstation/temperatures.xlsx'
 """ --------------------------------------------------------------------------------- """
 
+def update_screen(pin):
+    print("Light change detected")
+    time.sleep(0.1)
+
+    # if it's light in the room, wake up screen
+    if lightsensor.is_light():
+        print("It's light!")
+        if screen.is_sleeping():
+            screen.wake_up()
+            print("Woke up screen")
+    # If the screen is not sleeping, put it in sleep mode
+    else:
+        print("It's dark!")
+        if not screen.is_sleeping():
+            screen.go_to_sleep()
+            print("Put screen to sleep")
+
+
 if __name__ == "__main__":
     try:
         """ Setup of all the components """
@@ -31,6 +50,9 @@ if __name__ == "__main__":
 
         station = WeatherStation(DHT22_pin, API_key)
         lightsensor = LightSensor(lightsensor_pin)
+
+        """ Setup interrupt for when light changes """
+        GPIO.add_event_detect(lightsensor_pin, GPIO.RISING, callback=update_screen, bouncetime=200)
 
         """ Main loop """
         while True:
@@ -55,20 +77,11 @@ if __name__ == "__main__":
 
             # If it's light in the room, show temperature on screen
             if lightsensor.is_light():
-                # If the screen is sleeping, wake it up first
-                if screen.is_sleeping():
-                    screen.wake_up()
-
                 # get the icon image to display
                 icon_path = "icons/{}.bmp".format(icon[:2])
                 img = Image.open(icon_path)
                 # Display current temperatures and icon
                 screen.display(str(round(outside_temp)), str(round(inside_temp)), img)
-            # If it's dark
-            else:
-                # If the screen is not sleeping, put it in sleep mode
-                if not screen.is_sleeping():
-                    screen.go_to_sleep()
 
             print("-----------------------------------------------------------")
             # sleep 5 minutes
